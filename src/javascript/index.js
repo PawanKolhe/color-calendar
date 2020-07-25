719d27a4ac5eb717520b94c469b85ce14716fb8f
Pawan Kolhe <contact@pawankolhe.com>
2020-07-25 23:50:27 +0530

    reset START_WEEKDAY to 0

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,42 +3,42 @@ import '../sass/styles.scss';
 
 /**=================== INITIALIZE ===================**/
 
-const START_WEEKDAY = 2;  // Sun-0, Mon-1, Tue-2, Wed-3, Thu-4, Fri-5, Sat-6
+const START_WEEKDAY = 0;  // Sun-0, Mon-1, Tue-2, Wed-3, Thu-4, Fri-5, Sat-6
 const DAYS_TO_DISPLAY = 42;
 const today = new Date();
 
-let daysInPrevMonth = [];
-let daysInCurrentMonth = [];
-let daysInNextMonth = [];
+let daysIn_PrevMonth = [];
+let daysIn_CurrentMonth = [];
+let daysIn_NextMonth = [];
 
 /**=================== GENERATE DAYS ===================**/
 
 let currentDate = new Date();
 
 // Previous Month
-let firstDayPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1).getDay();
-let numOfDaysPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-for (let i = 0; i < numOfDaysPrevMonth; i++) {
-  daysInPrevMonth.push({ day: i + 1, selected: false });
+let firstDay_PrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1).getDay();
+let numOfDays_PrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+for (let i = 0; i < numOfDays_PrevMonth; i++) {
+  daysIn_PrevMonth.push({ day: i + 1, selected: false });
 }
 
 // Current Month
-let firstDayCurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
-let numOfDaysCurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
-for (let i = 0; i < numOfDaysCurrentMonth; i++) {
-  daysInCurrentMonth.push({ day: i + 1, selected: false });
+let firstDay_CurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+let numOfDays_CurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+for (let i = 0; i < numOfDays_CurrentMonth; i++) {
+  daysIn_CurrentMonth.push({ day: i + 1, selected: false });
 }
 
 // Next Month
-let firstDayNextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1).getDay();
-let numOfDaysNextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-for (let i = 0; i < numOfDaysNextMonth; i++) {
-  daysInNextMonth.push({ day: i + 1, selected: false });
+let firstDay_NextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1).getDay();
+let numOfDays_NextMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+for (let i = 0; i < numOfDays_NextMonth; i++) {
+  daysIn_NextMonth.push({ day: i + 1, selected: false });
 }
 
-// console.log(firstDayPrevMonth, numOfDaysPrevMonth, daysInPrevMonth);
-// console.log(firstDayCurrentMonth, numOfDaysCurrentMonth, daysInCurrentMonth);
-// console.log(firstDayNextMonth, numOfDaysNextMonth, daysInNextMonth);
+// console.log(firstDay_PrevMonth, numOfDays_PrevMonth, daysIn_PrevMonth);
+// console.log(firstDay_CurrentMonth, numOfDays_CurrentMonth, daysIn_CurrentMonth);
+// console.log(firstDay_NextMonth, numOfDays_NextMonth, daysIn_NextMonth);
 
 /**=================== RENDER WEEKDAYS ===================**/
 
@@ -60,22 +60,22 @@ let insertCount = 0;
 
 // Weekday Offset calculation
 let dayOffset;
-if(firstDayCurrentMonth < START_WEEKDAY) {
-  dayOffset =  7 + (firstDayCurrentMonth - START_WEEKDAY);
+if(firstDay_CurrentMonth < START_WEEKDAY) {
+  dayOffset =  7 + (firstDay_CurrentMonth - START_WEEKDAY);
 } else {
-  dayOffset = firstDayCurrentMonth - START_WEEKDAY;
+  dayOffset = firstDay_CurrentMonth - START_WEEKDAY;
 }
 
 // Prev Month
 for(let i = 0; i < dayOffset; i++) {
   calendarDays.innerHTML += `
-    <div class="calendar_day calendar_day-other">${daysInPrevMonth[daysInPrevMonth.length - dayOffset + i].day}</div>
+    <div class="calendar_day calendar_day-other">${daysIn_PrevMonth[daysIn_PrevMonth.length - dayOffset + i].day}</div>
   `;
   insertCount++;
 }
 
 // Current Month
-daysInCurrentMonth.forEach(day => {
+daysIn_CurrentMonth.forEach(day => {
   calendarDays.innerHTML += `
     <div class="calendar_day ${day.day == today.getDate() ? 'calendar_day-today' : ''}">${day.day}</div>
   `;
@@ -85,6 +85,6 @@ daysInCurrentMonth.forEach(day => {
 // Next Month
 for(let i = 0; i < DAYS_TO_DISPLAY - insertCount; i++) {
   calendarDays.innerHTML += `
-    <div class="calendar_day calendar_day-other">${daysInNextMonth[i].day}</div>
+    <div class="calendar_day calendar_day-other">${daysIn_NextMonth[i].day}</div>
   `;
 }
