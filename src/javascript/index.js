858cbee76af761b8b2977e5da405a46a668b0515
Pawan Kolhe <contact@pawankolhe.com>
2020-07-25 23:34:45 +0530

    add starting weekday change capability

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,9 @@ import '../sass/styles.scss';
 
 /**=================== INITIALIZE ===================**/
 
-let today = new Date();
+const START_WEEKDAY = 2;  // Sun-0, Mon-1, Tue-2, Wed-3, Thu-4, Fri-5, Sat-6
+const DAYS_TO_DISPLAY = 42;
+const today = new Date();
 
 let daysInPrevMonth = [];
 let daysInCurrentMonth = [];
@@ -34,24 +36,40 @@ for (let i = 0; i < numOfDaysNextMonth; i++) {
   daysInNextMonth.push({ day: i + 1, selected: false });
 }
 
-console.log(firstDayPrevMonth, numOfDaysPrevMonth, daysInPrevMonth);
-console.log(firstDayCurrentMonth, numOfDaysCurrentMonth, daysInCurrentMonth);
-console.log(firstDayNextMonth, numOfDaysNextMonth, daysInNextMonth);
+// console.log(firstDayPrevMonth, numOfDaysPrevMonth, daysInPrevMonth);
+// console.log(firstDayCurrentMonth, numOfDaysCurrentMonth, daysInCurrentMonth);
+// console.log(firstDayNextMonth, numOfDaysNextMonth, daysInNextMonth);
 
-/**=================== RENDER ===================**/
+/**=================== RENDER WEEKDAYS ===================**/
+
+const calendarWeekdays = document.querySelector('.calendar__weekdays');
+
+const WEEKDAYS_1_CHAR = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+for(let i = 0; i < 7; i++) {
+  calendarWeekdays.innerHTML += `
+    <div class="calendar__weekday">${WEEKDAYS_1_CHAR[(i + START_WEEKDAY) % 7]}</div>
+  `;
+}
+
+/**=================== RENDER DAYS ===================**/
 
 const calendarDays = document.querySelector('.calendar__days');
 
-/**
- * Current Month days
- */
-
 let insertCount = 0;
 
+// Weekday Offset calculation
+let dayOffset;
+if(firstDayCurrentMonth < START_WEEKDAY) {
+  dayOffset =  7 + (firstDayCurrentMonth - START_WEEKDAY);
+} else {
+  dayOffset = firstDayCurrentMonth - START_WEEKDAY;
+}
+
 // Prev Month
-for(let i = 0; i < firstDayCurrentMonth; i++) {
+for(let i = 0; i < dayOffset; i++) {
   calendarDays.innerHTML += `
-    <div class="calendar_day color-light">${daysInPrevMonth[daysInPrevMonth.length - firstDayCurrentMonth + i].day}</div>
+    <div class="calendar_day calendar_day-other">${daysInPrevMonth[daysInPrevMonth.length - dayOffset + i].day}</div>
   `;
   insertCount++;
 }
@@ -65,8 +83,8 @@ daysInCurrentMonth.forEach(day => {
 });
 
 // Next Month
-for(let i = 0; i < 42 - insertCount; i++) {
+for(let i = 0; i < DAYS_TO_DISPLAY - insertCount; i++) {
   calendarDays.innerHTML += `
-    <div class="calendar_day color-light">${daysInNextMonth[i].day}</div>
+    <div class="calendar_day calendar_day-other">${daysInNextMonth[i].day}</div>
   `;
 }
