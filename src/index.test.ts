cec58a7c3d40f18909353338ae61f33421ee25f1
Pawan Kolhe <contact@pawankolhe.com>
2020-12-07 16:40:15 +0530

    test: should initialize calendar

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,54 +1,83 @@
 import Calendar from './index';
 import { CalendarOptions } from "./types.d";
 
+import * as picker from "./modules/picker/picker";
+import { addEventListeners } from "./modules/addEventListeners";
+import { configureStylePreferences } from "./modules/stylePreference";
+
+jest.mock('./modules/picker/picker');
+jest.mock('./modules/addEventListeners');
+jest.mock('./modules/stylePreference');
+
+const DEFAULT_ID = '#color-calendar';
+
 beforeEach(() => {
-  // Inserts clean DIVs with id before each test
+  // Inserts clean DIVs with id to the DOM before each test
   document.body.innerHTML = `
     <div id="MyTestCalendar"></div>
     <div id="color-calendar"></div>
   `;
 });
 
-test('creates calendar with correct id', () => {
-  const id = '#MyTestCalendar';
-  const myCalendar = new Calendar({ id });
-  const calendarHTMLElement = document.querySelector(`${id} .color-calendar`);
+test('should initialize calendar', () => {
+  const mockTogglePicker = picker.togglePicker as jest.MockedFunction<typeof picker.togglePicker>;
+  const mockConfigureStylePreferences = configureStylePreferences as jest.MockedFunction<typeof configureStylePreferences>;
+  const mockAddEventListeners = addEventListeners as jest.MockedFunction<typeof addEventListeners>;
+  jest.spyOn(Calendar.prototype, 'reset');
 
-  expect(calendarHTMLElement).not.toBeNull();
+  const myCalendar = new Calendar();
+
+  expect(myCalendar.calendar).not.toBeNull();
+  expect(mockTogglePicker).toBeCalledTimes(1);
+  expect(mockConfigureStylePreferences).toBeCalledTimes(1);
+  expect(mockAddEventListeners).toBeCalledTimes(1);
+  expect(myCalendar.reset).toBeCalledTimes(1);
 });
 
-
-describe('calendar has default options when instantiated', () => {
+describe('default calendar options when instantiated', () => {
   let myCalendar;
-  let calendarHTMLElement: HTMLElement;
+  let calendarHTMLElement: HTMLElement | null;
 
   beforeEach(() => {
     myCalendar = new Calendar();
-    calendarHTMLElement = document.querySelector('#color-calendar') as HTMLElement;
+    calendarHTMLElement = document.querySelector(DEFAULT_ID)?.firstElementChild as HTMLElement;
   });
 
   test('default option id', () => {
-    expect(calendarHTMLElement?.firstElementChild?.classList).toContain('color-calendar--large');
+    calendarHTMLElement = document.querySelector(`${DEFAULT_ID} .color-calendar`);
+    expect(calendarHTMLElement).not.toBeNull();
   });
 
   test('default option calendarSize', () => {
-    expect(calendarHTMLElement?.firstElementChild?.classList).toContain('color-calendar--large');
+    expect(calendarHTMLElement?.classList).toContain('color-calendar--large');
   });
 
   test('default option layoutModifiers', () => {
-    expect(calendarHTMLElement?.firstElementChild?.classList).not.toContain('month-left-align');
+    expect(calendarHTMLElement?.classList).not.toContain('month-left-align');
+  });
+
+  test('default option monthDisplayType', () => {
+    expect(calendarHTMLElement?.classList).not.toContain('month-left-align');
   });
 });
 
+describe('custom calendar options when instantiated', () => {
+  
+  test('creates calendar', () => {
+    const id = '#MyTestCalendar';
+    const myCalendar = new Calendar({ id });
+    const calendarHTMLElement = document.querySelector(`${id} .color-calendar`);
+    
+    expect(calendarHTMLElement).not.toBeNull();
+  });
 
-describe('custom calendar options passed when instantiated', () => {
   test('custom option calendarSize', () => {
     const options: CalendarOptions = {
       calendarSize: 'small'
     };
     const myCalendar = new Calendar(options);
-    const calendarHTMLElement = document.querySelector('#color-calendar');
-
-    expect(calendarHTMLElement?.firstElementChild?.classList).toContain('color-calendar--small');
+    const calendarHTMLElement = document.querySelector(`${DEFAULT_ID} .color-calendar`);
+    
+    expect(calendarHTMLElement?.classList).toContain('color-calendar--small');
   });
 });
