a14d19ee5c18581f10d9e69815b9a7e51509e8a0
cherrol <575128052@qq.com>
2023-07-12 19:58:00 +0800

    chore: update bundler config

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,53 @@
-const path = require('path');
+const path = require("path");
 
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-import banner from 'rollup-plugin-banner';
-import typescript from '@rollup/plugin-typescript';
-import strip from '@rollup/plugin-strip';
+import banner from "rollup-plugin-banner";
+import typescript from "@rollup/plugin-typescript";
+import strip from "@rollup/plugin-strip";
+
+const isProd = process.env.mode === "production";
 
 const config = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "dist/bundle.cjs.js",
-        format: "cjs",
-      },
-      {
-        file: "dist/bundle.esm.js",
-        format: "esm",
-      },
-      {
-        name: "Calendar",
-        file: "dist/bundle.js",
-        format: "umd",
-      },
-    ],
-    plugins: [
-      typescript(),
-      strip(),  // removes console.log
-      commonjs({
-        include: "node_modules/**",
-      }),
-      resolve(),
-      babel({
-        exclude: "node_modules/**",
-        babelHelpers: "bundled",
-      }),
-      terser(), // minify javascript
-      banner('color-calendar\nv<%= pkg.version %>\nby <%= pkg.author %>'),
-    ],
-  },
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "dist/bundle.cjs.js",
+				format: "cjs",
+			},
+			{
+				file: "dist/bundle.esm.js",
+				format: "esm",
+			},
+			{
+				name: "Calendar",
+				file: "dist/bundle.js",
+				format: "umd",
+			},
+		],
+		plugins: [
+			typescript(),
+			strip(), // removes console.log
+			commonjs({
+				include: "node_modules/**",
+			}),
+			resolve(),
+			babel({
+				exclude: "node_modules/**",
+				babelHelpers: "bundled",
+			}),
+			banner("color-calendar\nv<%= pkg.version %>\nby <%= pkg.author %>"),
+		].concat(
+			isProd
+				? [
+						terser(), // minify javascript
+				  ]
+				: []
+		),
+	},
 ];
 
 export default config;
